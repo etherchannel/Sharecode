3ddd64843659558c5fe7da923ad4d30ca3985c4c
etherchannel <ping.rob@live.com>
2024-11-06 20:47:42 -0500

    rs

--- python/omevv15_vcenter_registration.py
+++ python/omevv15_vcenter_registration.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+requests.packages.urllib3.disable_warnings()
+
 ome_ip = 'demo-omevv-01-ome.ose.adc.delllabs.net'
 ome_username = 'rob_smith1@ose.local'
 ome_password = 'P@ssw0rd!23'
@@ -8,22 +10,33 @@ vcenter_ip = 'demo-omevv-01-vcsa.ose.adc.delllabs.net'
 vcenter_username = "administrator@vsphere.local"
 vcenter_password = "L@bT3@m>C0vid"
 
-url = f"https://{ome_ip}/omevv/GatewayService/v1/Consoles"
+def register_vcenter() -> str:
+    url = f"https://{ome_ip}/omevv/GatewayService/v1/Consoles"
 
-payload = json.dumps({
-  "extensions": [
-    "WEBCLIENT_PHA",
-    "VLCM"
-  ],
-  "credential": {
-    "username": vcenter_username,
-    "password": vcenter_password
-  },
-  "consoleAddress": vcenter_ip,
-  "description": "Registering VC"
-})
-headers = {'Content-Type': 'application/json'}
+    payload = json.dumps({
+    "consoleAddress": vcenter_ip,
+    "description": "",
+    "credential": {
+        "username": vcenter_username,
+        "domain": "",
+        "password": vcenter_password
+    },
+    "extensions": [
+        "WEBCLIENT",
+        "PHA",
+        "VLCM"
+    ],
+    "disableCNcheck": True
+    })
+    headers = {'Content-Type': 'application/json'}
 
-response = requests.post(url, headers=headers, data=payload, verify=False, auth=(vcenter_username, vcenter_password))
+    response = requests.post(url, headers=headers, data=payload, verify=False, auth=(ome_username, ome_password))
+    print(f"Attempting to register vCenter {vcenter_ip}")
 
-print(response.text)
+    if response.status_code == 201:
+        print(f"Successfully registered vCenter")
+    else:
+        print(response.text)
+        raise Exception(f"Registration failed with status code: {response.status_code}")
+
+register_vcenter()
